2cde29021952c19992275e8c057ef802a1ad6717
appgod2 <a0933611432@gmail.com>
2019-08-01 13:02:52 +0800

    add all

--- app.py
+++ app.py
@@ -13,10 +13,12 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('FlmQQSiEiaKaqoW1Pp3RWKa7I4Qbd39tUDBpod9v6vl6y0VkDelvqRKUFSXeO+IhT95usgs4IKt/j0i9qQQ82kjkymL4owhn1T0OMcEC1VSkO8Iuk4b7ApxFRjPC/QHskjulWjPaNWwah8JMj4GGNAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('c9bbe032d7640d2adcbb5e27cb28b012')
+myId=''
+getId=False
 
 @app.route('/')
 def index():
-    push_message()
+    
     return 'Hello World!'
 
 # @app.route('/<name>')
@@ -42,15 +44,24 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    global myId
+    global getId
+    if not getId:
+       myId=event.source.user_id
+       getId=True
+    
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="你好"))
 
 
+@app.route('/sendmsg')
 def push_message():
-    line_bot_api.push_message(
-        '0916598383',
-        TextSendMessage(text="我是機器人"))
+    try:
+        line_bot_api.push_message(
+            myId,
+            TextSendMessage(text="我是機器人"))
+    except Exception:
 
 if __name__=="__main__":
     app.run()
