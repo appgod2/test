bedf3b03383c892a1496264f2cbfa9d024c5c23c
appgod2 <a0933611432@gmail.com>
2019-08-01 14:30:29 +0800

    add all

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def handle_message(event):
     
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message))
+        TextSendMessage(text=event.message.Text))
 
 
 @app.route('/sendmsg')
